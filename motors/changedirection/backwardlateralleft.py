41a4f536cbd12dfd132402d30013b3af12ff9f45
ltlengg <ltlengg@connect.hku.hk>
2025-04-14 20:40:24 +0800

    T-2

--- motors/changedirection/backwardlateralleft.py
+++ motors/changedirection/backwardlateralleft.py
@@ -6,8 +6,8 @@ motorA = MotorA()
 motorD = MotorD()
 
 def backward_lateral_clockwise(speed,duration):
-	motorA.backward()
-	motorD.forward()
+	motorA.forward()
+	motorD.backward()
 	motorA.pwm.ChangeDutyCycle(speed)
 	motorD.pwm.ChangeDutyCycle(speed)
 	print(f"Changing backward direction to left at {speed}% speed for {duration} seconds")
--- motors/changedirection/backwardlateralright.py
+++ motors/changedirection/backwardlateralright.py
@@ -6,8 +6,8 @@ motorA = MotorA()
 motorD = MotorD()
 
 def backward_lateral_anticlockwise(speed,duration):
-	motorA.forward()
-	motorD.backward()
+	motorA.backward()
+	motorD.forward()
 	motorA.pwm.ChangeDutyCycle(speed)
 	motorD.pwm.ChangeDutyCycle(speed)
 	print(f"Changing backward direction to right at {speed}% speed for {duration} seconds")
