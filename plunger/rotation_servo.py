ad9704a70608dd8f021c3ee6bf4a124a7b10d89e
ltlengg <ltlengg@connect.hku.hk>
2025-04-15 15:14:38 +0800

    Servo control test 1

--- plunger/rotation_servo.py
+++ plunger/rotation_servo.py
@@ -14,12 +14,7 @@ def servo_control():
         servo.pwm.ChangeDutyCycle(sig)  
         time.sleep(0.03)
     servo.pwm.stop()'''
-    for i in range(0,3):
-        servo.pwm.ChangeDutyCycle(2.0) # rotate to 0 degrees
-        time.sleep(0.5)
-        servo.pwm.ChangeDutyCycle(12.0) # rotate to 180 degrees
-        time.sleep(0.5)
-        servo.pwm.ChangeDutyCycle(7.0) # rotate to 90 degrees
-        time.sleep(0.5)
+    servo.pwm.ChangeDutyCycle(2.0) # rotate to 0 degrees
+    time.sleep(0.5)
     servo.pwm.ChangeDutyCycle(0)
     servo.pwm.stop()
