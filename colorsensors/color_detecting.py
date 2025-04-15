853ad01f6d989031640754fec5de36357d176eed
ltlengg <ltlengg@connect.hku.hk>
2025-04-15 18:19:02 +0800

    call class instance error fix

--- colorsensors/color_detecting.py
+++ colorsensors/color_detecting.py
@@ -5,6 +5,10 @@ from colorsensors.csB import Color_SensorB
 from colorsensors.csC import Color_SensorC
 from colorsensors.colorfilter import red, blue, green
 
+colorsensorA = Color_SensorA()
+colorsensorB = Color_SensorB()
+colorsensorC = Color_SensorC()
+
 cycle = 10
 
 def color_detecting():
@@ -13,7 +17,7 @@ def color_detecting():
         time.sleep(0.1)
         start = time.time()
         for impulse_count in range(cycle):
-            io.wait_for_edge(Color_SensorA.out, io.FALLING)
+            io.wait_for_edge(colorsensorA.out, io.FALLING)
         duration = time.time() - start
         red_value = cycle / duration
         print(f"R:{red_value}")
@@ -22,7 +26,7 @@ def color_detecting():
         time.sleep(0.1)
         start = time.time()
         for impulse_count in range(cycle):
-            io.wait_for_edge(Color_SensorA.out, io.FALLING)
+            io.wait_for_edge(colorsensorA.out, io.FALLING)
         duration = time.time() - start
         blue_value = cycle / duration
         print(f"B:{blue_value}")
@@ -31,7 +35,7 @@ def color_detecting():
         time.sleep(0.1)
         start = time.time()
         for impulse_count in range(cycle):
-            io.wait_for_edge(Color_SensorA.out, io.FALLING)
+            io.wait_for_edge(colorsensorA.out, io.FALLING)
         duration = time.time() - start
         green_value = cycle / duration
         print(f"G:{green_value}")
--- colorsensors/colorfilter.py
+++ colorsensors/colorfilter.py
@@ -3,26 +3,30 @@ from colorsensors.csA import Color_SensorA
 from colorsensors.csB import Color_SensorB
 from colorsensors.csC import Color_SensorC
 
+colorsensorA = Color_SensorA()
+colorsensorB = Color_SensorB()
+colorsensorC = Color_SensorC()
+
 def red():
-    io.output(Color_SensorA.s2, io.LOW)
-    io.output(Color_SensorA.s3, io.LOW)
-    io.output(Color_SensorB.s2, io.LOW)
-    io.output(Color_SensorB.s3, io.LOW)
-    io.output(Color_SensorC.s2, io.LOW)
-    io.output(Color_SensorC.s3, io.LOW)
+    io.output(colorsensorA.s2, io.LOW)
+    io.output(colorsensorA.s3, io.LOW)
+    io.output(colorsensorB.s2, io.LOW)
+    io.output(colorsensorB.s3, io.LOW)
+    io.output(colorsensorC.s2, io.LOW)
+    io.output(colorsensorC.s3, io.LOW)
 
 def blue():
-    io.output(Color_SensorA.s2, io.HIGH)
-    io.output(Color_SensorA.s3, io.HIGH)
-    io.output(Color_SensorB.s2, io.HIGH)
-    io.output(Color_SensorB.s3, io.HIGH)
-    io.output(Color_SensorC.s2, io.HIGH)
-    io.output(Color_SensorC.s3, io.HIGH)
+    io.output(colorsensorA.s2, io.HIGH)
+    io.output(colorsensorA.s3, io.HIGH)
+    io.output(colorsensorB.s2, io.HIGH)
+    io.output(colorsensorB.s3, io.HIGH)
+    io.output(colorsensorC.s2, io.HIGH)
+    io.output(colorsensorC.s3, io.HIGH)
 
 def green():
-    io.output(Color_SensorA.s2, io.LOW)
-    io.output(Color_SensorA.s3, io.HIGH)
-    io.output(Color_SensorB.s2, io.LOW)
-    io.output(Color_SensorB.s3, io.HIGH)
-    io.output(Color_SensorC.s2, io.LOW)
-    io.output(Color_SensorC.s3, io.HIGH)
+    io.output(colorsensorA.s2, io.LOW)
+    io.output(colorsensorA.s3, io.HIGH)
+    io.output(colorsensorB.s2, io.LOW)
+    io.output(colorsensorB.s3, io.HIGH)
+    io.output(colorsensorC.s2, io.LOW)
+    io.output(colorsensorC.s3, io.HIGH)
