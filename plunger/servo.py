6afef81720f8499e17ea2be10925e7507136c09b
ltlengg <ltlengg@connect.hku.hk>
2025-04-15 11:07:10 +0800

    Attribute Error Correction

--- plunger/servo.py
+++ plunger/servo.py
@@ -2,14 +2,14 @@ import RPi.GPIO as io
 
 class Servo():
     def __init__(self,pwm=8,vcc=10,grd=12):
-        self.pwm = pwm
+        self.pwm_pin = pwm
         self.vcc = vcc
         self.grd = grd
-        io.setup(self.pwm,io.OUT)
+        io.setup(self.pwm_pin,io.OUT)
         io.setup(self.vcc,io.OUT)
         io.setup(self.grd,io.OUT)
         io.output(self.vcc, io.HIGH)
         io.output(self.grd, io.LOW)
         if not hasattr(Servo, 'pwm'):
-            Servo.pwm = io.PWM(self.pwm, 50)
+            Servo.pwm = io.PWM(self.pwm_pin, 50)
             Servo.pwm.start(0)
