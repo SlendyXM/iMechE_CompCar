e13d31713ac4bed3aa14aebb89f237dbc25b2fe1
ltlengg <ltlengg@connect.hku.hk>
2025-04-16 21:51:44 +0800

    detect color conditions updated

--- colorsensors/color_detecting.py
+++ colorsensors/color_detecting.py
@@ -3,70 +3,81 @@ import time
 from colorsensors.csA import Color_SensorA
 #from colorsensors.csB import Color_SensorB
 #from colorsensors.csC import Color_SensorC
+#from colorsensors.csD import Color_SensorD
 from colorsensors.colorfilter import red, blue, green
 
 colorsensorA = Color_SensorA()
 #colorsensorB = Color_SensorB()
 #colorsensorC = Color_SensorC()
+#colorsensorD = Color_SensorD()
 
 cycle = 10
 
+def detect_color(sensor, sensor_name):
+    red()
+    time.sleep(0.05)
+    start = time.time()
+    for impulse_count in range(cycle):
+        io.wait_for_edge(sensor.out, io.FALLING)
+    duration = time.time() - start
+    red_value = cycle / duration
+
+    blue()
+    time.sleep(0.05)
+    start = time.time()
+    for impulse_count in range(cycle):
+        io.wait_for_edge(sensor.out, io.FALLING)
+    duration = time.time() - start
+    blue_value = cycle / duration
+
+    green()
+    time.sleep(0.05)
+    start = time.time()
+    for impulse_count in range(cycle):
+        io.wait_for_edge(sensor.out, io.FALLING)
+    duration = time.time() - start
+    green_value = cycle / duration
+
+    print(f"{sensor_name} - R:{red_value:.2f} G:{green_value:.2f} B:{blue_value:.2f}")
+
+    if red_value >= 300 and green_value >= 300 and blue_value >= 300:
+        print(f"{sensor_name} - white")
+        return "white"
+    elif red_value <= 300 and green_value <= 300 and blue_value <= 300:
+        print(f"{sensor_name} - black")
+    elif red_value < blue_value and green_value < blue_value and blue_value >= 300:
+        print(f"{sensor_name} - blue")
+    elif red_value >= 300 and green_value < red_value and blue_value < red_value:
+        print(f"{sensor_name} - red")
+    else:
+        print(f"{sensor_name} - wood")
+
 def color_detecting():
-    temp = 1
-    while (1):
-        red()
-        time.sleep(0.05)
-        start = time.time()
-        for impulse_count in range(cycle):
-            io.wait_for_edge(colorsensorA.out, io.FALLING)
-        duration = time.time() - start
-        red_value = cycle / duration
+    while True:
+        # Detect color for Sensor A
+        color_a = detect_color(colorsensorA, "Color Sensor A")
+        print(f"Detected by Color Sensor A: {color_a}")
 
-        blue()
-        time.sleep(0.05)
-        start = time.time()
-        for impulse_count in range(cycle):
-            io.wait_for_edge(colorsensorA.out, io.FALLING)
-        duration = time.time() - start
-        blue_value = cycle / duration
+        '''# Detect color for Sensor B
+        color_b = detect_color(colorsensorB, "Color Sensor B")
+        print(f"Detected by Color Sensor B: {color_b}")
 
-        green()
-        time.sleep(0.05)
-        start = time.time()
-        for impulse_count in range(cycle):
-            io.wait_for_edge(colorsensorA.out, io.FALLING)
-        duration = time.time() - start
-        green_value = cycle / duration
-        
-        print(f"R:{red_value: .2f} G:{green_value: .2f} B:{blue_value: .2f}")
+        # Detect color for Sensor C
+        color_c = detect_color(colorsensorC, "Color Sensor C")
+        print(f"Detected by Color Sensor C: {color_c}")
 
-        if red_value >= 300 and green_value >= 300 and blue_value >= 300:
-            temp += 1
-            print("white")
-            if temp > 4:
-                print("white")
-                temp = 1
-        elif red_value <= 300 and green_value <= 300 and blue_value <= 300:
-            temp += 1
-            print("black")
-            if temp > 4:
-                print("black")
-                temp = 1
-        elif red_value < blue_value and green_value < blue_value and blue_value >= 300:
-            temp += 1
-            print("blue")
-            if temp > 4:
-                print("blue")
-                temp = 1
-        elif red_value >= 300 and green_value < red_value and blue_value < red_value:
-            temp += 1
-            print("red")
-            if temp > 4:
-                print("red")
-                temp = 1
-        else:
-            temp += 1
-            print("wood")
-            if temp > 4:
-                print("wood")
-                temp = 1
+        # Detect color for Sensor D
+        color_d = detect_color(colorsensorD, "Color Sensor D")
+        print(f"Detected by Color Sensor D: {color_d}")'''
+
+        # Check the specified conditions
+        if color_a == "red": # and color_b == "blue" and color_c == "blue" and color_d == "black":
+            print("Condition met: Sensor A is red, Sensor B and C are blue, Sensor D is black.")
+            return True  # Break the loop and return True
+
+        # If conditions are not met, output False
+        print("Condition not met. Returning False.")
+        return False
+
+
+
