20f399523e9d45e7d7652684fd62f3ea7a383db2
ltlengg <ltlengg@connect.hku.hk>
2025-04-16 22:36:42 +0800

    opened a camera folder

--- main.py
+++ main.py
@@ -56,9 +56,12 @@ motor_c_pins = [22, 24, 26]
 motor_d_pins = [36, 38, 40]
 plunger_pin = [8]
 color_sensor_a_pins = [29, 31, 32, 33, 35, 37]
+color_sensor_b_pins = []
+color_sensor_c_pins = []
+color_sensor_d_pins = []
 
 # Combine all pins into a single list
-all_pins = stby_pin + motor_a_pins + motor_b_pins + motor_c_pins + motor_d_pins + plunger_pin + color_sensor_a_pins
+all_pins = stby_pin + motor_a_pins + motor_b_pins + motor_c_pins + motor_d_pins + plunger_pin + color_sensor_a_pins + color_sensor_b_pins + color_sensor_c_pins + color_sensor_d_pins
 
 # Main Function
 def main():
