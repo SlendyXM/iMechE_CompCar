7b33bbe059f872df08cdcf437827c0bf14118132
ltlengg <ltlengg@connect.hku.hk>
2025-04-18 21:29:44 +0800

    push

--- leds/green.py
+++ leds/green.py
@@ -1,10 +1,13 @@
-import RPi.GPIO as io
-import time
-
+from ..extension_gpio_board import execute_device_command
+#     # execute_device_command(command_index=2, input_array=[5, 1])  # Set pin 5 to high
+    
 def LED_Green(power):
     pwm_pin = 42
-    io.setup(pwm_pin, io.OUT)
     if power == True:
-        io.output(pwm_pin, io.HIGH)
+        execute_device_command(command_index=2, input_array=[pwm_pin, 1])
     else:
-        io.output(pwm_pin, io.LOW)
+        execute_device_command(command_index=2, input_array=[pwm_pin, 0])
+
+if __name__ == "__main__":
+    LED_Green(True)
+    LED_Green(False)
