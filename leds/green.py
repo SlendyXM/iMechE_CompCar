fd1208fdb55eca8a97a7f52e12c49754efcc26f9
ltlengg <ltlengg@connect.hku.hk>
2025-04-18 21:30:38 +0800

    push

--- leds/green.py
+++ leds/green.py
@@ -1,4 +1,4 @@
-from ..extension_gpio_board import execute_device_command
+from .extension_gpio_board import execute_device_command
 #     # execute_device_command(command_index=2, input_array=[5, 1])  # Set pin 5 to high
     
 def LED_Green(power):
